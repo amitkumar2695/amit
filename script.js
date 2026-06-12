e06557214bfd08971eaf2a1e6be70c664e22241f
amitkumar2695 <kumar.amit261995@gmail.com>
2026-06-13 00:25:26 +0200

    Add openPositionTab function to manage tab states

--- script.js
+++ script.js
@@ -15,3 +15,28 @@ function openTab(evt, tabName) {
   document.getElementById(tabName).classList.add("active");
   evt.currentTarget.classList.add("active");
 }
+function openPositionTab(evt, tabName) {
+
+    let i;
+
+    let tabcontent =
+    document.getElementsByClassName("position-subtab");
+
+    for(i=0;i<tabcontent.length;i++) {
+
+        tabcontent[i].classList.remove("active");
+    }
+
+    let tablinks =
+    document.getElementsByClassName("subtab-btn");
+
+    for(i=0;i<tablinks.length;i++) {
+
+        tablinks[i].classList.remove("active");
+    }
+
+    document.getElementById(tabName)
+    .classList.add("active");
+
+    evt.currentTarget.classList.add("active");
+}
